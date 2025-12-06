6bfa74560996ab9b3f953cba55ce5ca3d0ee0016
A5R13L <asriel@asriel.dev>
2025-12-06 08:55:57 -0500

    fix builder

--- build.sh
+++ build.sh
@@ -13,14 +13,14 @@ fi
 if [ "$PRESET" == "x86_64" ]; then
     CONFIG="releasewithsymbols_x86_64"
     CXXFLAGS="-std=c++20"
-    BUILD_PATH="projects/x64/linux/gmake2"
-    ARTIFACT_PATH="projects/x64/linux/gmake2/x86_64/ReleaseWithSymbols/gmsv_apakr_64_linux64.dll"
+    BUILD_PATH="projects/apakr_64/linux/gmake2"
+    ARTIFACT_PATH="projects/apakr_64/linux/gmake2/x86_64/ReleaseWithSymbols/gmsv_apakr_64_linux64.dll"
     ARTIFACT_NAME="gmsv_apakr_plugin_64.so"
 else
     CONFIG="releasewithsymbols_x86"
     CXXFLAGS="-std=c++17"
-    BUILD_PATH="projects/x32/linux/gmake2"
-    ARTIFACT_PATH="projects/x32/linux/gmake2/x86/ReleaseWithSymbols/gmsv_apakr_32_linux.dll"
+    BUILD_PATH=" projects/apakr_32/linux/gmake2"
+    ARTIFACT_PATH="projects/apakr_32/linux/gmake2/x86/ReleaseWithSymbols/gmsv_apakr_32_linux.dll"
     ARTIFACT_NAME="gmsv_apakr_plugin_32.so"
 fi
 
