536c45c2254d74dce8ae49d1ebdba7f1f3d584ae
A5R13L <asriel@asriel.dev>
2025-12-06 08:41:11 -0500

    refactor workflow?

---
+++ build.ps1
@@ -0,0 +1,41 @@
+param(
+    [string]$Preset = ""
+)
+
+if ($Preset -eq "") {
+    $Arch = $env:PROCESSOR_ARCHITECTURE
+
+    if ($Arch -eq "AMD64" -or $Arch -eq "x64") {
+        $Preset = "x86_64"
+    }
+    else {
+        $Preset = "x86"
+    }
+}
+
+Invoke-WebRequest -Uri 'https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-windows.zip' -OutFile premake5.zip
+Expand-Archive premake5.zip -DestinationPath premake5
+premake5/premake5.exe vs2022
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg
+.\bootstrap-vcpkg.bat
+
+if ($Preset -eq "x86_64") {
+    .\vcpkg.exe install curl[ssl]:x64-windows-static
+}
+else {
+    .\vcpkg.exe install curl[ssl]:x86-windows-static
+}
+
+.\vcpkg.exe integrate install
+cd ..
+$Env:LINK='crypt32.lib secur32.lib'
+
+if ($Preset -eq "x86_64") {
+    cd projects/x64/windows/vs2022
+    msbuild apakr_64.sln /p:Configuration=releasewithsymbols /p:Platform=x64 /p:VcpkgTriplet=x64-windows-static
+}
+else {
+    cd projects/x32/windows/vs2022
+    msbuild apakr_32.sln /p:Configuration=releasewithsymbols /p:VcpkgTriplet=x86-windows-static
+}
---
+++ build.sh
@@ -0,0 +1,34 @@
+if [ -n "$1" ]; then
+    PRESET="$1"
+else
+    ARCH=$(uname -m)
+
+    if [[ "$ARCH" == "x86_64" ]]; then
+        PRESET="x86_64"
+    else
+        PRESET="x86"
+    fi
+fi
+
+if [[ "$PRESET" == "x86_64" ]]; then
+    CONFIG="releasewithsymbols_x86_64"
+    CXXFLAGS="-std=c++20"
+    BUILD_PATH="projects/x64/linux/gmake2"
+    ARTIFACT_PATH="projects/x64/linux/gmake2/x86_64/ReleaseWithSymbols/gmsv_apakr_64_linux64.dll"
+    ARTIFACT_NAME="gmsv_apakr_plugin_64.so"
+else
+    CONFIG="releasewithsymbols_x86"
+    CXXFLAGS="-std=c++17"
+    BUILD_PATH="projects/x32/linux/gmake2"
+    ARTIFACT_PATH="projects/x32/linux/gmake2/x86/ReleaseWithSymbols/gmsv_apakr_32_linux.dll"
+    ARTIFACT_NAME="gmsv_apakr_plugin_32.so"
+fi
+
+if [[ "$ARCH" == "x86" ]]; then
+    dpkg --add-architecture i386
+fi
+
+apt-get update && apt-get install -y build-essential ninja-build curl zip unzip tar pkg-config wget git gcc-multilib g++-multilib libcurl4-openssl-dev cmake make
+premake5 gmake2
+cd $BUILD_PATH
+make config=$CONFIG CXX=g++-10 CXXFLAGS="$CXXFLAGS" LDFLAGS="-lpthread -lcurl"
